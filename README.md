7262277ce8271e42340b6858f200d8550de60493
Nicolás Salazar <nicolas.salazar@ncodes.co>
2020-05-26 14:10:36 -0500

    Se agrega mockup con todos los posts cargados a Firestore

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ## CLI:
 **Para instanciar server de pruebas a nivel local**:
 
-firebase serve --project gram-1f446 
+firebase serve --project gram-1f446
+firebase serve --project gram-dev-fd70b
 
 **Para despliegue de las funcitons**:
 
 firebase deploy --only functions --project gram-1f446
+firebase deploy --only functions --project gram-dev-fd70b
--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,35 @@ const functions = require('firebase-functions');
 
 //Declaración de utilidades de firestore:
 var admin = require("firebase-admin");
+// var serviceAccount = require('./gram-dev-fd70b-firebase-adminsdk-231eh-ce974a1146.json');
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://gram-dev-fd70b.firebaseio.com"
+//   });
+
 var serviceAccount = require('./4avux-dae4159740.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://gram-1f446.firebaseio.com"
 });
 
+
 //Utilidad de base de datos:
 const firestoreDB = admin.firestore();
+
+exports.obtenerTodosLosPosts = functions.https.onCall(async (data, context) => {
+    return (
+        new Promise((resolve, reject) => {
+            firestoreDB.collection("posts").orderBy("date", "desc").get()
+                .then(snapshots => {
+                    let postsList = [];
+                    snapshots.forEach(snapshot => { postsList.push({ id: snapshot.id, ...snapshot.data() }) });
+                    resolve({ "status": "SUCCESS", "posts": postsList });
+                })
+                .catch(error => { reject({ "status": "FAILED", "error": error.toString() }); });
+        })
+    );
+});
 
 exports.obtenerPosts = functions.https.onCall(async (data, context) => {
     return (

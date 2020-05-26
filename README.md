72016cf6a4857c95e99a6825ba771aafc13cb079
Nicolás Salazar <nicolas.salazar@ncodes.co>
2020-05-26 12:31:00 -0500

    Se crean los microservicios para cargue de posts y obtencion de posts con paginacion

---
+++ README.md
@@ -0,0 +1,8 @@
+## CLI:
+**Para instanciar server de pruebas a nivel local**:
+
+firebase serve --project gram-1f446 
+
+**Para despliegue de las funcitons**:
+
+firebase deploy --only functions --project gram-1f446
---
+++ functions/index.js
@@ -0,0 +1,40 @@
+const functions = require('firebase-functions');
+
+//Declaración de utilidades de firestore:
+var admin = require("firebase-admin");
+var serviceAccount = require('./4avux-dae4159740.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://gram-1f446.firebaseio.com"
+});
+
+//Utilidad de base de datos:
+const firestoreDB = admin.firestore();
+
+exports.obtenerPosts = functions.https.onCall(async (data, context) => {
+    return (
+        new Promise((resolve, reject) => {
+            firestoreDB.collection("posts").orderBy("date", "desc").offset(data.offset).limit(data.limit).get()
+                .then(snapshots => {
+                    let postsList = [];
+                    snapshots.forEach(snapshot => { postsList.push({ id: snapshot.id, ...snapshot.data() }) });
+                    resolve({ "status": "SUCCESS", "posts": postsList });
+                })
+                .catch(error => { reject({ "status": "FAILED", "error": error.toString() }); });
+        })
+    );
+});
+
+exports.crearPost = functions.https.onCall(async (data, context) => {
+    return (
+        new Promise((resolve, reject) => {
+            firestoreDB.collection("posts").add({ ...data, date: new Date() })
+                .then(() => { resolve({ "status": "SUCCESS" }); })
+                .catch(error => { reject({ "status": "FAILED", "error": error.toString() }); });
+        })
+    );
+});
+
+exports.funcionDeEjemplo = functions.https.onCall(async (data, context) => {
+    return ("Hola " + data.nombre + ". Esta es una función de ejemplo que al parecer funciona 🧐");
+});
